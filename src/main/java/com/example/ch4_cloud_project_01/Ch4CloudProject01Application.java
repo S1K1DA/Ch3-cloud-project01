1e0a32b78c7e53608b51fe33eaf521682b9698ed
EUNSIK JUNG <bamtol2100@naver.com>
2026-01-29 18:21:19 +0900

    first commit

---
+++ src/main/java/com/example/ch4_cloud_project_01/Ch4CloudProject01Application.java
@@ -0,0 +1,13 @@
+package com.example.ch4_cloud_project_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch4CloudProject01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch4CloudProject01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ch4_cloud_project_01/Ch4CloudProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch4_cloud_project_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch4CloudProject01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
